34b8f1911dfb80869b898f210cfe4bfd1a7767fa
asteria <asteria@syvita.org>
2021-09-10 18:18:32 +0100

    WE GOT A WORKING WORKER

--- packages/worker/test/handler.test.ts
+++ packages/worker/test/handler.test.ts
@@ -4,15 +4,15 @@ import makeServiceWorkerEnv from 'service-worker-mock'
 declare var global: any
 
 describe('handle', () => {
-    beforeEach(() => {
-        Object.assign(global, makeServiceWorkerEnv())
-        jest.resetModules()
-    })
+  beforeEach(() => {
+    Object.assign(global, makeServiceWorkerEnv())
+    jest.resetModules()
+  })
 
-    test('handle GET', async () => {
-        const result = await handleRequest(new Request('/', { method: 'GET' }))
-        expect(result.status).toEqual(200)
-        const text = await result.text()
-        expect(text).toEqual('request method: GET')
-    })
+  test('handle GET', async () => {
+    const result = await handleRequest(new Request('/', { method: 'GET' }))
+    expect(result.status).toEqual(200)
+    const text = await result.text()
+    expect(text).toEqual('request method: GET')
+  })
 })
