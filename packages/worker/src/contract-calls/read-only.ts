9e73edda6ef802870dc430fefadc051c43b96089
asteria <asteria@syvita.org>
2021-09-11 13:35:23 +0100

    more

--- packages/worker/src/contract-calls/read-only.ts
+++ packages/worker/src/contract-calls/read-only.ts
@@ -1,12 +1,12 @@
 import { callReadOnlyFunction } from 'micro-stacks/transactions'
 import { getReadonlyTxOptions } from '../lib'
-import { uintCV } from 'micro-stacks/clarity'
+import { ClarityValue, uintCV } from 'micro-stacks/clarity'
 
 export async function getFirstIncompleteRound(): Promise<number> {
   return 1
 }
 
-export async function getRoundStatus(roundId:number): Promise<object> {
+export async function getRoundStatus(roundId:number): Promise<ClarityValue> {
     const options = getReadonlyTxOptions([uintCV(roundId)], 'get-round-status')
     return await callReadOnlyFunction(options)
 }
