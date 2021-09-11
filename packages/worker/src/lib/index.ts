492d92bfb59f33617834fd60e5620ed423d94b29
asteria <asteria@syvita.org>
2021-09-11 15:02:58 +0100

    new deploy

--- packages/worker/src/lib/index.ts
+++ packages/worker/src/lib/index.ts
@@ -7,7 +7,7 @@ export const MIAMIPOOL_ADDY = 'ST343J7DNE122AVCSC4HEK4MF871PW470ZV04CFXH'
 export const API_SERVER = 'https://stacks-node-api.testnet.stacks.co'
 // export const API_SERVER = 'https://stacks-node-api.mainnet.stacks.co'
 
-export const MIAMIPOOL_NAME = 'optimistic-orange-angelfish'
+export const MIAMIPOOL_NAME = 'wet-aqua-anglerfish'
 
 export const NETWORK = new StacksTestnet()
 // export const NETWORK = new StacksMainnet()
