073a19546b89c8f3254220dc99c3c913e92b0284
asteria <asteria@syvita.org>
2021-09-15 13:51:38 +0100

    tie worker to latest

--- packages/worker/src/lib/index.ts
+++ packages/worker/src/lib/index.ts
@@ -7,7 +7,7 @@ export const MIAMIPOOL_ADDY = 'ST343J7DNE122AVCSC4HEK4MF871PW470ZV04CFXH'
 export const API_SERVER = 'https://stacks-node-api.testnet.stacks.co'
 // export const API_SERVER = 'https://stacks-node-api.mainnet.stacks.co'
 
-export const MIAMIPOOL_NAME = 'purring-tan-tarantula'
+export const MIAMIPOOL_NAME = 'stale-teal-eel'
 
 export const MATURITY_LEN = 11
 // export const MATURITY_LEN = 100
