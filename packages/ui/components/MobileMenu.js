8bf9199c53eb38691ec2e3a0e89813ee2952f16b
SyInvidia <paradinight@protonmail.com>
2021-09-16 13:58:18 +0100

    does this fix it part 2

--- packages/ui/components/MobileMenu.js
+++ packages/ui/components/MobileMenu.js
@@ -1,6 +1,6 @@
 import styles from '../styles/MobileMenu.module.css';
 import { useState } from 'react';
-import NavLinks from '../components/Navlinks';
+import NavLinks from './NavLinks';
 
 const MobileMenu = () => {
 
