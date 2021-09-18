d1008289ab2d716d56d46202baff4dec8c26f915
SyInvidia <paradinight@protonmail.com>
2021-09-18 11:13:25 +0100

    update UI to use new contract

--- packages/ui/lib/constants.js
+++ packages/ui/lib/constants.js
@@ -2,8 +2,8 @@ import { StacksTestnet } from '@stacks/network'
 
 export const NETWORK = new StacksTestnet()
 export const GENESIS_CONTRACT_ADDRESS = 'SP000000000000000000002Q6VF78'
-export const MIAMIPOOL_CONTRACT_ADDRESS = 'ST343J7DNE122AVCSC4HEK4MF871PW470ZV04CFXH'
-export const MIAMIPOOL_CONTRACT_NAME = 'soviet-orange-pelican'
+export const MIAMIPOOL_CONTRACT_ADDRESS = 'ST2J2ASASFAS80NGCVP2CVKDCSPR2GF2DQG9V5E3H'
+export const MIAMIPOOL_CONTRACT_NAME = 'severe-amethyst-catfish'
 export const MIAMICOIN_CONTRACT_ADDRESS = 'ST3CK642B6119EVC6CT550PW5EZZ1AJW6608HK60A'
 export const MIAMICOIN_CONTRACT_NAME = 'citycoin-core-v4'
 export const API_SERVER = 'https://stacks-node-api.testnet.stacks.co'
